1abaa82099acea40819adfd2bdaf855867043f5e
Echo Lee <bdottn.lee+git@gmail.com>
2024-12-19 17:25:38 +0800

    【Issue：#1】整數相除
    1. 測試類別：DivisionTests
        * 建立測試案例：Divide_WhenDivisorIsZero_ThrowsDivideByZeroException_1
        * 建立測試案例：Divide_WhenDivisorIsZero_ThrowsDivideByZeroException_1
        * 建立測試案例：Divide_ExtremeInput_CorrectCalculation

--- TDD.Arithnetic.Tests.Unit.xUnit/DivisionTests.cs
+++ TDD.Arithnetic.Tests.Unit.xUnit/DivisionTests.cs
@@ -29,5 +29,49 @@
             // Assert
             Assert.Equal(1.5m, quotient);
         }
+
+        [Fact]
+        public void Divide_WhenDivisorIsZero_ThrowsDivideByZeroException_1()
+        {
+            // Arrange
+            int dividend = 1;
+            int divisor = 0;
+
+            // Act
+            Exception e = Record.Exception(() => Division.Divide(dividend, divisor));
+
+            // Assert
+            Assert.IsType<DivideByZeroException>(e);
+        }
+
+        [Fact]
+        public void Divide_WhenDivisorIsZero_ThrowsDivideByZeroException_2()
+        {
+            // Arrange
+            int dividend = 1;
+            int divisor = 0;
+
+            // Act
+            Action act = () => Division.Divide(dividend, divisor);
+
+            // Assert
+            Assert.Throws<DivideByZeroException>(act);
+        }
+
+        [Theory]
+        [InlineData(int.MaxValue, int.MinValue, -0.999999999534)]
+        [InlineData(-int.MaxValue, int.MinValue, 0.999999999534)]
+        [InlineData(int.MinValue, int.MaxValue, -1.000000000466)]
+        [InlineData(int.MinValue, -int.MaxValue, 1.000000000466)]
+        public void Divide_ExtremeInput_CorrectCalculation(int dividend, int divisor, decimal expectedQuotient)
+        {
+            // Arrange
+
+            // Act
+            decimal actQuotient = Division.Divide(dividend, divisor);
+
+            // Assert
+            Assert.Equal(expectedQuotient, actQuotient, 12);
+        }
     }
 }
